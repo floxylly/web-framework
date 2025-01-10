0d75029b003f2e008923b52f6e5734cb810d1400
Alma sakhiragama khairunisa <121219230+floxylly@users.noreply.github.com>
2025-01-10 21:55:41 +0700

    create.blade.php

--- resources/views/Movies/create.blade.php
+++ resources/views/Movies/create.blade.php
@@ -21,12 +21,12 @@
     </div>
 
     <div class="grid md:grid-cols-2 md:gap-6">
-      <!-- genre -->
+      <!-- Genre -->
       <div class="relative z-0 w-full mb-5 group">
         <select name="genre" id="genre"
           class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           required>
-          <option value="" disabled {{ old('genre') ? '' : 'selected' }}>Pilih kategori</option>
+          <option value="" disabled {{ old('genre') ? '' : 'selected' }}>Pilih genre</option>
           @foreach($genre as $genre)
           <option value="{{ $genre->id }}" {{ old('genre') == $genre->id ? 'selected' : '' }}>
             {{ $genre->name }}
@@ -41,28 +41,28 @@
         @enderror
       </div>
 
-      <!-- Stock -->
+      <!-- Year -->
       <div class="relative z-0 w-full mb-5 group">
-        <input type="number" name="stock" id="stock" value="{{ old('stock') }}" min="0"
+        <input type="number" name="year" id="year" value="{{ old('year') }}" min="0"
           class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
           placeholder=" " required />
-        <label for="stock"
-          class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Stock</label>
-        @error('stock')
+        <label for="year"
+          class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Year</label>
+        @error('year')
         <!-- Tampilkan error validasi -->
         <span class="text-red-500 text-sm">{{ $message }}</span>
         @enderror
       </div>
     </div>
 
-    <!-- Summary -->
+    <!-- Synopsis -->
     <div class="relative z-0 w-full mb-5 group">
-      <textarea name="summary" id="summary" rows="4"
+      <textarea name="synopsis" id="synopsis" rows="4"
         class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-        placeholder=" " required>{{ old('summary') }}</textarea>
-      <label for="summary"
-        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Summary</label>
-      @error('summary')
+        placeholder=" " required>{{ old('synopsis') }}</textarea>
+      <label for="synopsis"
+        class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Synopsis</label>
+      @error('synopsis')
       <!-- Tampilkan error validasi -->
       <span class="text-red-500 text-sm">{{ $message }}</span>
       @enderror
